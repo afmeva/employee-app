b7812a44b9a5bac0ea54cc92b3f702442c5fb7c9
Andrés Medina <andresfelipe@andres-medina.local>
2018-07-05 16:00:30 -0500

    Install ngrx store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
-export interface employee {
-  id: number;
-  name: string;
-  age: number;
-  username: string;
-  hireDate: number;
-}
-
-const ELEMENT_DATA: employee[] = [
-  { id: 1, name: 'Pikachu', age: 10, username: 'SexyThunder', hireDate: 1800000 },
-  { id: 2, name: 'Squirtle', age: 10, username: 'DeadPool', hireDate: 1800000 },
-  { id: 3, name: 'Goku', age: 10, username: 'Mona123', hireDate: 1800000 },
-];
+interface AppState {
+  employees: object[];
+};
 
 @Component({
   selector: 'app-root',
@@ -20,5 +12,9 @@ const ELEMENT_DATA: employee[] = [
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  data = ELEMENT_DATA;
+  data: Observable<object>;
+
+  constructor(private store: Store<AppState>) {
+    this.data = store.pipe(select('employees'));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { EmployeeTableComponent } from './employee-table/employee-table.component';
+
+export interface employee {
+  id: number;
+  name: string;
+  age: number;
+  username: string;
+  hireDate: number;
+}
+
+const ELEMENT_DATA: employee[] = [
+  { id: 1, name: 'Pikachu', age: 10, username: 'SexyThunder', hireDate: 1800000 },
+  { id: 2, name: 'Squirtle', age: 10, username: 'DeadPool', hireDate: 1800000 },
+  { id: 3, name: 'Goku', age: 10, username: 'Mona123', hireDate: 1800000 },
+];
 
 @NgModule({
   declarations: [
@@ -12,6 +27,9 @@ import { EmployeeTableComponent } from './employee-table/employee-table.componen
   ],
   imports: [
     BrowserAnimationsModule,
+    StoreModule.forRoot({
+      employees: () => ELEMENT_DATA
+    }),
     MatTableModule
   ],
   providers: [],
