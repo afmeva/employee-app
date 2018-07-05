36a6ca81b6400824bcba08c5a069f38130b7074f
Andrés Medina <andresfelipe@andres-medina.local>
2018-07-05 17:48:28 -0500

    Create initial unit test for employee-table component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,32 @@
+import { Component, Input } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 import { TestBed, async } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
+
+@Component({ selector: 'app-employee-table', template: '' })
+class tableMockComponent {
+  @Input() dataSource;
+}
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        tableMockComponent
+      ],
+      imports: [
+        StoreModule.forRoot({
+          employees: () => []
+        })
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to employee-app!');
   }));
 });
--- src/app/employee-table/employee-table.component.spec.ts
+++ src/app/employee-table/employee-table.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatTableModule } from '@angular/material/table';
 
 import { EmployeeTableComponent } from './employee-table.component';
 
@@ -6,20 +7,56 @@ describe('EmployeeTableComponent', () => {
   let component: EmployeeTableComponent;
   let fixture: ComponentFixture<EmployeeTableComponent>;
 
+  const DATA = [
+    { id: 1, name: 'Pikachu', age: 10, username: 'SexyThunder', hireDate: 1800000 },
+    { id: 2, name: 'Squirtle', age: 10, username: 'DeadPool', hireDate: 1800000 },
+    { id: 3, name: 'Goku', age: 10, username: 'Mona123', hireDate: 1800000 },
+  ];
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EmployeeTableComponent ]
+      declarations: [EmployeeTableComponent],
+      imports: [MatTableModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EmployeeTableComponent);
     component = fixture.componentInstance;
+
+    component.dataSource = DATA;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the expected rows with the given data', () => {
+    expect(fixture.nativeElement.querySelectorAll('tr[mat-row]').length).toBe(DATA.length)
+  });
+
+  it('should render the expected buttons with the given button array', () => {
+    const BUTTON_ARRAY = [
+      {
+        name: 'edit',
+        callback: jasmine.createSpy()
+      },
+      {
+        name: 'erase',
+        callback: jasmine.createSpy()
+      }
+    ];
+
+    const buttons = fixture.nativeElement.querySelector('td[mat-cell]').querySelectorAll('button');
+    expect(buttons.length).toBe(BUTTON_ARRAY.length);
+
+    buttons[0].click();
+    expect(buttons[0].callback).toHaveBeenCalled();
+
+    buttons[1].click();
+    expect(buttons[1].callback).toHaveBeenCalled();
   });
 });
